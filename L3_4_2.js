82194d6396013c5d2c8647345df1e2217e9496c4
duhe <lucciv@163.com>
2018-08-14 14:02:24 +0800

    Add some js files

--- L3_4_2.js
+++ L3_4_2.js
@@ -1,2 +1,14 @@
 'use strict';
-console.log('Hello,My Mac!');
+
+function make_pow(n) {
+	return function(x) {
+		return Math.pow(x, n);
+	}
+}
+
+// 创建两个新函数:
+var pow2 = make_pow(2);
+var pow3 = make_pow(3);
+
+console.log(pow2(5)); // 25
+console.log(pow3(5)); // 343
